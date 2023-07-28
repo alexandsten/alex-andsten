e0c7155a01b19704b2ff4e648234d7704076e667
Alex A <alex.andsten@gmail.com>
2023-07-28 14:59:21 +0200

    component fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,11 @@ function App() {
   return (
     <>
     <Header />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Alex Portfolio</h1>
-      <div className="card">
-       <AboutMe />
-       <MyProjects />
+      <div>
+        <AboutMe />
+        <MyProjects />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <Footer />
     </>
   )
---
+++ src/components/ContactLinks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactLinks = () => {
+  return (
+    <div>
+      <p>Länk till github</p>
+      <p>Länk till Linkedin</p>
+      <p>Mailaddress</p>
+      <p>Öppna / ladda ner CV? kanske onödig</p>
+    </div>
+  )
+}
+
+export default ContactLinks
