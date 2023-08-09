0f95dc4b2c09a36cf61af392b335798b0ad7dc25
Alex A <alex.andsten@gmail.com>
2023-08-09 16:34:25 +0200

    menulinks

--- src/components/MenuNav.jsx
+++ src/components/MenuNav.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const MenuNav = () => {
+  const openWindow = (url) => {
+    window.open(url, "_blank", "noreferrer");
+  };
   return (
     <div>
-      <p>Meny - work in progess</p>
+      <ul id='mainMenu'>
+        <li>Om mig</li>
+        <li>Projekt</li>
+        <li onClick={() => openWindow("https://www.linkedin.com/in/alex-andstén-47937116b")}>Linkedin</li>
+        <li onClick={() => openWindow("https://github.com/alexandsten")}>Github</li>
+      </ul>
     </div>
   )
 }
