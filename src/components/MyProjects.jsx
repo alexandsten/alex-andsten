0d12b2491eabf1dd12523a045e0d21ad216f9104
Alex A <alex.andsten@gmail.com>
2023-08-10 13:36:25 +0200

    link

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -4,6 +4,7 @@ const MyProjects = () => {
   return (
     <div id='myProjects'>
       <a href="https://storied-sundae-b1d64e.netlify.app/" target='_blank'>Film och spel API</a>
+      <a href="https://super-medovik-6d5ee3.netlify.app/" target='_blank'>MMA AI projekt</a>
     </div>
   )
 }
