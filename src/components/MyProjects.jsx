119be7cbfef4c45f46d8e4fcfccf02874b468d8b
Alex A <alex.andsten@gmail.com>
2023-08-10 14:04:08 +0200

    projekt link

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const MyProjects = () => {
   return (
     <div id='myProjects'>
-      <a href="https://storied-sundae-b1d64e.netlify.app/" target='_blank'>Film och spel API</a>
-      <a href="https://frosty-ramanujan-ec2af0.netlify.app/" target='_blank'>Javascript spel</a>
+      <a href="https://storied-sundae-b1d64e.netlify.app/" target='_blank'>Film och spel API</a> <br />
+      <a href="https://frosty-ramanujan-ec2af0.netlify.app/" target='_blank'>Javascript spel</a> <br />
+      <a href="https://gleeful-alfajores-115146.netlify.app/" target='_blank'>Hänga gubbe</a> <br />
       <a href="https://super-medovik-6d5ee3.netlify.app/" target='_blank'>MMA AI projekt</a>
     </div>
   )
