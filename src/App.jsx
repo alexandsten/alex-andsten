672fd35504f6091c688111f84e5ec8628a332a28
Alex A <alex.andsten@gmail.com>
2023-08-10 15:31:20 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,30 @@ function App() {
     <>
     <Header />
       {/* Smooth scrolling links */}
-      <div>
-        <Link to="section1" smooth={true} duration={500}>Go to Section 1</Link>
-        <Link to="section2" smooth={true} duration={500}>Go to Section 2</Link>
+      <div id='mainNav'>
+        <Link to="section1" smooth={true} duration={500}>Mina kunskaper</Link>
+        <Link to="section2" smooth={true} duration={500}>Mina projekt</Link>
+      </div>
+      <div className='para'>
+      <p>Jag är en student på Jensen Yrkeshögskola som studerar frontendutveckling. Jag söker
+    efter en praktikplats som frontendutvecklare.
+    Under min utbildning har jag lärt mig att skapa användarvänliga och estetiskt tilltalande
+    gränssnitt för webbplatser och webbapplikationer. Jag har lärt mig att använda
+    frontendteknologier som HTML, CSS, JavaScript och ramverk som React för att skapa
+    dynamiska och responsiva webbapplikationer.
+    </p>
+    <p>Jag är entusiastisk över att få möjligheten att tillämpa mina färdigheter och kunskaper
+    på en spännande och innovativ praktikplats. Jag är också beredd att lära mig nya
+    teknologier och arbetssätt för att fortsätta utvecklas som frontendutvecklare.
+    Jag tror att min passion för teknologi och mitt engagemang för att skapa
+    användarvänliga gränssnitt och webbapplikationer skulle göra mig till en värdefull
+    tillgång för teamet. Jag ser fram emot att diskutera hur jag kan bidra till företaget under
+    min praktikperiod.</p>
+    
+
       </div>
   
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      
       <Element name="section1">
         <AboutMe />
       </Element>
