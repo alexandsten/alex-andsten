bd7b67d9ebca6e4826644b44cb103fe974e03b74
Alex A <alex.andsten@gmail.com>
2023-08-11 16:16:48 +0200

    fix

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -5,6 +5,7 @@ const MyProjects = () => {
     <div id='myProjects'>
       <h2>Mina projekt</h2>
       <a href="https://storied-sundae-b1d64e.netlify.app/" target='_blank'>Film och spel API</a> <br />
+      <a href="https://dapper-longma-6212ba.netlify.app/" target='_blank'>Film shop</a> <br />
       <a href="https://frosty-ramanujan-ec2af0.netlify.app/" target='_blank'>Javascript spel</a> <br />
       <a href="https://gleeful-alfajores-115146.netlify.app/" target='_blank'>Hänga gubbe</a> <br />
       <a href="https://super-medovik-6d5ee3.netlify.app/" target='_blank'>MMA AI projekt</a>
