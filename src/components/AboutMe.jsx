77204b1d2f575fe8172580f810d7ad9d9979e079
Alex A <alex.andsten@gmail.com>
2024-07-24 20:12:13 +0200

    new start

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,31 +1,57 @@
+import { Stack, Typography } from '@mui/material'
 import React from 'react'
+import pulse from 'react-animations/lib/pulse';
+import styled, { keyframes } from 'styled-components';
 
 const AboutMe = () => {
+
+  const pulseAnimation = keyframes`${pulse}`;
+
+  const PulseDiv = styled.div`
+    animation: 2s ${pulseAnimation} infinite;
+  `;
   return (
-    <div id='aboutMe' className='para'>
-      <h2>Utbildning och kunskaper</h2>
-      <b>Frontendutvecklare, JENSEN yrkeshögskola</b>
-    <p>
-      2022-pågående
+   
+      <Stack id='aboutMe' className='para'>
+
+      <PulseDiv>
+        <h2>Utbildning och kunskaper</h2>
+      </PulseDiv>
+
+      <Typography>
+        Frontendutvecklare, JENSEN yrkeshögskola
+      </Typography>
+
+      <Typography>
+        2022-pågående
       - Javascript, HTML, CSS, ramverk, react, react native, jsx, databashantering,
       arbete med API’er, grundläggande UX, testning, agilt projektmetodik
-    </p>
-    <b>Interaktiva medier och webbteknologier, Linnéuniversitetet</b>
-
-    <p>
+      </Typography>
+    
+   
+    <Typography>
+      Interaktiva medier och webbteknologier, Linnéuniversitetet
+    </Typography>
+  
+    <Typography>
       2020-2021
       - Javascript, HTML, CSS, PHP, lärande av klient - server - databas,
       phpMyAdmin, SQL, MySQL
-    </p>
-    <b>3-årig IT/media gymnasieutbildning, John Bauer</b>
-
-    <p>
-      2008-2011
+    </Typography>
+    
+    <Typography>
+    3-årig IT/media gymnasieutbildning, John Bauer
+    </Typography>
+   
+      <Typography>
+        2008-2011
       -HTML, CSS, Adobe paketet, bild retuschering
-    </p>
-      
-      <p>Mina färdigheter - javascript osv</p>
-    </div>
+      </Typography>
+      <Typography>
+        Mina färdigheter - javascript osv
+      </Typography>
+      </Stack>
+    
   )
 }
 
