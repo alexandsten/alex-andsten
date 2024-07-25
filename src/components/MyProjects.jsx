b0580975f73dfbefb51f232b00ee7f383dc97cc7
Alex A <alex.andsten@gmail.com>
2024-07-25 20:33:18 +0200

    new start

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,18 +1,43 @@
 import { Stack } from '@mui/material'
 import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const MyProjects = () => {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
   return (
+    <>
       <Stack height={'75vh'} width={'99vw'} sx={{ backgroundColor: 'pink'}}
         justifyContent={'center'} alignItems={'center'}
       >
+
         <h2>Mina projekt</h2>
-        <a href="https://storied-sundae-b1d64e.netlify.app/" target='_blank'>Film och spel API</a> <br />
-        <a href="https://dapper-longma-6212ba.netlify.app/" target='_blank'>Film shop</a> <br />
-        <a href="https://frosty-ramanujan-ec2af0.netlify.app/" target='_blank'>Javascript spel</a> <br />
-        <a href="https://gleeful-alfajores-115146.netlify.app/" target='_blank'>Hänga gubbe</a> <br />
-        <a href="https://super-medovik-6d5ee3.netlify.app/" target='_blank'>MMA AI projekt</a>
+
+        <Stack width={'50%'} mt={5}>
+         <Slider {...settings} >
+          <Stack>
+            <Stack height={'10em'} justifyContent={'center'} alignItems={'center'}>
+              <a href="https://storied-sundae-b1d64e.netlify.app/" target='_blank'>Nostradamage</a> 
+            </Stack>
+            
+          </Stack>
+          <Stack>
+            <Stack height={'10em'} justifyContent={'center'} alignItems={'center'}>
+            <a href="https://dapper-longma-6212ba.netlify.app/" target='_blank'>Nobel Light Week</a>
+            </Stack>
+          </Stack>
+        </Slider> 
+        </Stack>
+        
       </Stack>
+    </>
   )
 }
 
