317476f3c12f20dd071f9c712b90bfe68317b082
Alex A <alex.andsten@gmail.com>
2024-08-22 19:49:45 +0200

    project images start

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -50,7 +50,7 @@ const MyProjects = () => {
         <Stack height={'75vh'} justifyContent={'center'} alignItems={'center'} flexDirection={'row'} gap={3}
           sx={{ border: '3px solid black', borderRadius: '15px' }}
         >
-          <Grid container sx={{ height: '100%'}}>
+          <Grid container sx={{ height: '100%', width: '100%'}}>
             <Grid item md={6} sm={12} sx={{
               backgroundImage: `url(${nostraPic1})`,
               backgroundSize: 'cover',
@@ -61,14 +61,21 @@ const MyProjects = () => {
               {/* The left side with the background image */}
             </Grid>
             <Grid item md={6} sm={12}>
-              <Stack height={'100%'} justifyContent={'space-between'} alignItems={'center'} p={3}
-                sx={{ borderTopRightRadius: '15px', borderBottomRightRadius: '15px' }}
+              <Stack height={'91%'} width={'91%'} justifyContent={'space-between'} alignItems={'center'} p={3}
+                sx={{ borderTopRightRadius: '15px', borderBottomRightRadius: '15px', backgroundColor: '#f4f7e9' }}
               >
-                <Typography>Predictions startup for MMA</Typography>
-                <Typography>Nostradamage</Typography>
-                <a href="https://nostradamage.netlify.app/" target="_blank" rel="noopener noreferrer">Besök webbplats</a>
+                <Stack p={3} mr={3} justifyContent={'center'} alignItems={'center'} sx={{ backgroundColor: 'white', height: '10%', padding: '3px', width: '75%', borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'}}>
+                  <Typography sx={{ fontFamily: "Gotu", fontSize: '24px', fontWeight: 'bold'}}>Nobel Week Lights</Typography>
+                </Stack>
+                <Stack p={3} mr={3} justifyContent={'center'} alignItems={'center'} sx={{ backgroundColor: 'white',  borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'}}>
+                  <Typography sx={{ fontFamily: "Gotu"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia molestiae error cupiditate dolorem eaque, nostrum ipsam ea exercitationem provident tempora veniam aliquid harum, facere, consectetur voluptate veritatis nihil porro necessitatibus.</Typography>
+                </Stack>
                 <PulseDiv>
-                  <Typography>Läs mer</Typography>
+                  <a href="https://nostradamage.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <Typography sx={{ fontFamily: "Gotu"}}>
+                      Läs mer
+                    </Typography>
+                  </a>
                 </PulseDiv>
               </Stack>
             </Grid>
@@ -81,7 +88,7 @@ const MyProjects = () => {
         <Stack height={'75vh'} justifyContent={'center'} alignItems={'center'} flexDirection={'row'} gap={3}
           sx={{ border: '3px solid black', borderRadius: '15px' }}
         >
-          <Grid container sx={{ height: '100%'}}>
+          <Grid container sx={{ height: '100%', width: '100%'}}>
             <Grid item md={6} sm={12} sx={{
               backgroundImage: `url(${nobelPic1})`,
               backgroundSize: 'cover',
@@ -92,14 +99,21 @@ const MyProjects = () => {
               {/* The left side with the background image */}
             </Grid>
             <Grid item md={6} sm={12}>
-              <Stack height={'100%'} justifyContent={'space-between'} alignItems={'center'} p={3}
-                sx={{ borderTopRightRadius: '15px', borderBottomRightRadius: '15px' }}
+              <Stack height={'91%'} width={'91%'} justifyContent={'space-between'} alignItems={'center'} p={3}
+                sx={{ borderTopRightRadius: '15px', borderBottomRightRadius: '15px', backgroundColor: '#f4f7e9' }}
               >
-                <Typography>Predictions startup for MMA</Typography>
-                <Typography>Nostradamage</Typography>
-                <a href="https://nostradamage.netlify.app/" target="_blank" rel="noopener noreferrer">Besök webbplats</a>
+                <Stack p={3} mr={3} justifyContent={'center'} alignItems={'center'} sx={{ backgroundColor: 'white', height: '10%', padding: '3px', width: '75%', borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'}}>
+                  <Typography sx={{ fontFamily: "Gotu", fontSize: '24px', fontWeight: 'bold'}}>Nobel Week Lights</Typography>
+                </Stack>
+                <Stack p={3} mr={3} justifyContent={'center'} alignItems={'center'} sx={{ backgroundColor: 'white',  borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'}}>
+                  <Typography sx={{ fontFamily: "Gotu"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia molestiae error cupiditate dolorem eaque, nostrum ipsam ea exercitationem provident tempora veniam aliquid harum, facere, consectetur voluptate veritatis nihil porro necessitatibus.</Typography>
+                </Stack>
                 <PulseDiv>
-                  <Typography>Läs mer</Typography>
+                  <a href="https://nostradamage.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <Typography sx={{ fontFamily: "Gotu"}}>
+                      Läs mer
+                    </Typography>
+                  </a>
                 </PulseDiv>
               </Stack>
             </Grid>
@@ -109,29 +123,35 @@ const MyProjects = () => {
 
       {/* Third Slide */}
       <Stack>
-        <Stack height={'75vh'} justifyContent={'space-between'} alignItems={'center'} flexDirection={'row'} gap={3}
+        <Stack height={'75vh'} justifyContent={'center'} alignItems={'center'} flexDirection={'row'} gap={3}
           sx={{ border: '3px solid black', borderRadius: '15px' }}
         >
-          <Grid container sx={{ height: '100%' }}>
+          <Grid container sx={{ height: '100%', width: '100%'}}>
             <Grid item md={6} sm={12} sx={{
               backgroundImage: `url(${hitractPic1})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
               borderTopLeftRadius: '15px',
               borderBottomLeftRadius: '15px',
-              borderRight: 'solid black 1px',
             }}>
               {/* The left side with the background image */}
             </Grid>
             <Grid item md={6} sm={12}>
-              <Stack height={'100%'} justifyContent={'space-between'} alignItems={'center'} p={3}
-                sx={{ borderTopRightRadius: '15px', borderBottomRightRadius: '15px' }}
+              <Stack height={'91%'} width={'91%'} justifyContent={'space-between'} alignItems={'center'} p={3}
+                sx={{ borderTopRightRadius: '15px', borderBottomRightRadius: '15px', backgroundColor: '#f4f7e9' }}
               >
-                <Typography>Predictions startup for MMA</Typography>
-                <Typography>Nostradamage</Typography>
-                <a href="https://nostradamage.netlify.app/" target="_blank" rel="noopener noreferrer">Besök webbplats</a>
+                <Stack p={3} mr={3} justifyContent={'center'} alignItems={'center'} sx={{ backgroundColor: 'white', height: '10%', padding: '3px', width: '75%', borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'}}>
+                  <Typography sx={{ fontFamily: "Gotu", fontSize: '24px', fontWeight: 'bold'}}>Nobel Week Lights</Typography>
+                </Stack>
+                <Stack p={3} mr={3} justifyContent={'center'} alignItems={'center'} sx={{ backgroundColor: 'white',  borderRadius: '15px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'}}>
+                  <Typography sx={{ fontFamily: "Gotu"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia molestiae error cupiditate dolorem eaque, nostrum ipsam ea exercitationem provident tempora veniam aliquid harum, facere, consectetur voluptate veritatis nihil porro necessitatibus.</Typography>
+                </Stack>
                 <PulseDiv>
-                  <Typography>Läs mer</Typography>
+                  <a href="https://nostradamage.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <Typography sx={{ fontFamily: "Gotu"}}>
+                      Läs mer
+                    </Typography>
+                  </a>
                 </PulseDiv>
               </Stack>
             </Grid>
