ce2841f9746ff9285604f7d66106e39102fcf5a6
Alex A <alex.andsten@gmail.com>
2024-09-14 17:14:55 +0200

    animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Link, Element } from 'react-scroll';
+import React, { useState, useEffect } from 'react';
+import { Link, Element, scroller } from 'react-scroll';
 import CV from './assets/AlexAndstenCV.pdf';
 import './App.css';
 import Footer from './components/Footer';
@@ -8,7 +9,7 @@ import fadeIn from 'react-animations/lib/fade-in';
 import styled, { keyframes } from 'styled-components';  
 import al3 from './img/transpbackground2.png';
 import whiteBg from './img/whiteBg.jpg';
-import { Stack, Typography, GlobalStyles } from '@mui/material';
+import { Stack, Typography, GlobalStyles, IconButton, Drawer, Grid } from '@mui/material';
 import { styled as muiStyled } from '@mui/system'; 
 import { useMediaQuery } from '@mui/material';
 import slideInRight from 'react-animations/lib/slide-in-right';
@@ -33,7 +34,6 @@ const FadeDiv = styled.div`
 `;
 
 function App() {
-
   const CustomTypography = muiStyled(Typography)(({ theme }) => ({
     '&::first-letter': {
       fontSize: '24px',
@@ -51,6 +51,25 @@ function App() {
   const isMiniScreen = useMediaQuery('(max-width:600px)');
   const isLargeScreen = useMediaQuery('(max-width:1400px)');
 
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    if (open === false) {
+      window.scrollTo(0, scrollPosition); // Restore scroll position when closing the drawer
+    }
+    setDrawerOpen(open);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => setScrollPosition(window.scrollY);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       <GlobalStyles
@@ -67,6 +86,18 @@ function App() {
           },
         }}
       />
+        <Stack width={'100%'} maxHeight={'0vh'} flexDirection={'row-reverse'} zIndex={10} 
+            sx={{
+            position: 'sticky',
+            top: 0,
+            zIndex: 10, 
+          }}>
+          <Stack m={2} pb={2} sx={{ backgroundColor: 'white', width: '3em', height: '3em', borderRadius: '50%'}}>
+            <IconButton onClick={toggleDrawer(true)} >
+              <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 512 512"><path d="M80 96h352v32H80zm0 144h352v32H80zm0 144h352v32H80z"/></svg>
+            </IconButton>
+          </Stack>
+        </Stack>
       <Stack
         maxWidth={'100vw'}
         minHeight={ isSmallScreen ? '33vh': '100vh'}
@@ -77,36 +108,15 @@ function App() {
           backgroundRepeat: 'no-repeat',
         }}
       >
-        <Stack minHeight={ isSmallScreen ? '33vh': '100vh'}  width={'100%'} flexDirection={'column'} sx={{
-                    backgroundImage: `url(${al3})`,
-                    backgroundSize: 'contain',
-                    backgroundPosition: 'center left',
-                    backgroundRepeat: 'no-repeat',
-                  }}  >
-          <Stack width={'100%'} flexDirection={'row-reverse'} position={'absolute'} zIndex={10}>
-            <Stack flexDirection={'column'} mt={4} gap={1}>
-              <Link to="section1" smooth={true} duration={500}>
-                <Typography sx={{ fontFamily: 'Gotu', fontSize: '18px' }}>
-                  Experience & skills
-                </Typography>
-              </Link>
-              <Link to="section2" smooth={true} duration={500}>
-                <Typography sx={{ fontFamily: 'Gotu', fontSize: '18px' }}>
-                  Work & projects
-                </Typography>
-              </Link>
-              <Link to="section3" smooth={true} duration={500}>
-                <Typography sx={{ fontFamily: 'Gotu', fontSize: '18px' }}>
-                  Contact
-                </Typography>
-              </Link>
-              <a href={CV} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none'}}>
-                <Typography sx={{ fontFamily: 'Gotu', fontSize: '18px' }}>
-                  CV in Swedish
-                </Typography>
-              </a>
-            </Stack>
-          </Stack>
+        <Stack minHeight={ isSmallScreen ? '33vh': '100vh'}  width={'100%'} flexDirection={'column'} 
+          sx={{
+              backgroundImage: `url(${al3})`,
+              backgroundSize: 'contain',
+              backgroundPosition: 'center left',
+              backgroundRepeat: 'no-repeat',
+            }}  
+          >
+          
 
           <Stack className="para" width={'100%'} height={'100%'} >
             <Stack
@@ -121,14 +131,12 @@ function App() {
               <Stack
                 width={'100%'}
                 height={'100%'}
-                
                 spacing={1}
                 pt={5}
                 position={'relative'}
               >
                 <Stack
                   flexDirection={'column'}
-                   
                   ml={5}
                   pt={3}
                   height={'100%'}
@@ -197,6 +205,38 @@ function App() {
           <Footer />
         </Element>
       </Stack>
+      <Drawer
+        anchor="right"
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+        sx={{ zIndex: 9999 }}
+      >
+        <Stack role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
+          <Grid container direction="column" spacing={2} sx={{ padding: '20px', backgroundColor: '#141414', height: '102vh', overflow: 'hidden' }}>
+            <Grid item>
+              <Link to="section1" smooth={true} duration={500} onClick={toggleDrawer(false)}>
+                <Typography sx={{ fontFamily: 'Gotu', fontSize: '18px', color: 'white' }}>
+                  Experience & skills
+                </Typography>
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link to="section3" smooth={true} duration={500} onClick={toggleDrawer(false)}>
+                <Typography sx={{ fontFamily: 'Gotu', fontSize: '18px', color: 'white' }}>
+                  Contact
+                </Typography>
+              </Link>
+            </Grid>
+            <Grid item>
+              <a href={CV} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none'}} onClick={toggleDrawer(false)}>
+                <Typography sx={{ fontFamily: 'Gotu', fontSize: '18px', color: 'white' }}>
+                  CV in Swedish
+                </Typography>
+              </a>
+            </Grid>
+          </Grid>
+        </Stack>
+      </Drawer>
     </>
   );
 }
