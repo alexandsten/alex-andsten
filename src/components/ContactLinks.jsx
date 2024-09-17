f05148e3ecf387827e4aaded4e6f7c838e4011fd
Alex A <alex.andsten@gmail.com>
2024-09-17 17:10:13 +0200

    footer

--- src/components/ContactLinks.jsx
+++ src/components/ContactLinks.jsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, Grid } from '@mui/material';
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
 import styled, { keyframes, css } from 'styled-components';
@@ -34,33 +34,52 @@ const ContactLinks = () => {
             color: 'white',
             fontFamily: 'Gotu',
             fontWeight: 'bold',
-            fontSize: '20px',
+            fontSize: '4em',
           }}
         >
           Lets get in touch!
         </Typography>
       </AnimatedTada>
-      <h3>Contact me</h3>
-      <a href="mailto:alex.andsten@hotmail.com">
-        <Typography sx={{ color: 'white', fontFamily: 'Gotu' }}>
-          alex.andsten@hotmail.com
+      {/* <h3>Contact me</h3> */}
+      <Stack width={'100%'} justifyContent={'center'} alignItems={'center'}>
+
+        <Grid container width={'75%'} spacing={3}>
+
+        <Grid item xs={12} md={6} xl={3}  justifyContent={'center'} alignItems={'center'}>
+          <a href="https://github.com/alexandsten" target="_blank">
+            <Typography sx={{ color: 'white', fontFamily: 'Gotu' }}>Github</Typography>
+          </a>
+        </Grid>
+        
+        <Grid item xs={12} md={6} xl={3}>
+          <Typography sx={{ color: 'white', fontFamily: 'Gotu' }}>
+            +46 76 260 82 27
+          </Typography>
+        </Grid>
+        
+        <Grid item xs={12} md={6} xl={3}>
+          <a href="https://linkedin.com/in/alex-andstén-47937116b" target="_blank">
+            <Typography sx={{ color: 'white', fontFamily: 'Gotu' }}>Linkedin</Typography>
+          </a>
+        </Grid>
+
+        <Grid item xs={12} md={6} xl={3}>
+          <a href="mailto:alex.andsten@hotmail.com">
+            <Typography sx={{ color: 'white', fontFamily: 'Gotu' }}>
+              alex.andsten@hotmail.com
+            </Typography>
+          </a>
+        </Grid>
+
+        </Grid>
+      </Stack>
+
+      <Stack width={'100%'} height={'20%'} justifyContent={'flex-end'} alignItems={'center'}>
+        <Typography sx={{ color: 'white', fontFamily: 'Gotu', fontSize: '8px' }}>
+            Background images Designed by Freepik
         </Typography>
-      </a>
-      <Typography sx={{ color: 'white', fontFamily: 'Gotu' }}>
-        076-260 82 27
-      </Typography>
+      </Stack>
 
-      <a href="https://linkedin.com/in/alex-andstén-47937116b" target="_blank">
-        <Typography sx={{ color: 'white', fontFamily: 'Gotu' }}>Linkedin</Typography>
-      </a>
-
-      <a href="https://github.com/alexandsten" target="_blank">
-        <Typography sx={{ color: 'white', fontFamily: 'Gotu' }}>Github</Typography>
-      </a>
-
-      <Typography sx={{ color: 'white', fontFamily: 'Gotu', fontSize: '8px' }}>
-        Background images Designed by Freepik
-      </Typography>
     </Stack>
   );
 };
